f59635b8fd79a18238423bcab54a39664dac3684
Anthony Fink <40363736+psytonik@users.noreply.github.com>
2024-03-30 14:09:37 +0300

    adding fonts

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import {createBoard} from "@/actions/createBoard";
+import {Button} from "@/components/ui/button";
+import {db} from "@/lib/db";
+import {DeleteBoard} from "@/app/(platform)/(dashboard)/organization/[organizationId]/board";
 
-const OrganizationIdPage = () => {
-
+const OrganizationIdPage = async ({params}:{params:{organizationId: string}}) => {
+	const orgId = params.organizationId;
+	const boards = await db.board.findMany();
 	return (
-		<div>
-			Org Page
+		<div className="flex flex-col space-y-4">
+			<form action={createBoard}>
+				<input type="text" name="title" id="title" required placeholder="Enter board Title" className="border-black border p-1"/>
+				<Button type="submit" variant="ghost">Submit</Button>
+			</form>
+			<div className="space-y-2">
+				{boards.map((board)=>(
+					<DeleteBoard id={board.id} title={board.title} key={board.id}/>
+				))}
+			</div>
 		</div>
 	);
 };
