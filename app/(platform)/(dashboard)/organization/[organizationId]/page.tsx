d171296a6d03c91acd51e287688a3d7034485813
Anthony Fink <40363736+psytonik@users.noreply.github.com>
2024-04-04 09:51:28 +0300

    adding new components / pages

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,22 +1,16 @@
 import React from 'react';
-import {createBoard} from "@/actions/createBoard";
-import {Button} from "@/components/ui/button";
-import {db} from "@/lib/db";
-import {DeleteBoard} from "@/app/(platform)/(dashboard)/organization/[organizationId]/board";
+import {Info} from "./_components/info";
+import {Separator} from "@/components/ui/separator";
+import BoardList from "@/app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list";
 
-const OrganizationIdPage = async ({params}:{params:{organizationId: string}}) => {
-	const orgId = params.organizationId;
-	const boards = await db.board.findMany();
+
+const OrganizationIdPage = async () => {
 	return (
-		<div className="flex flex-col space-y-4">
-			<form action={createBoard}>
-				<input type="text" name="title" id="title" required placeholder="Enter board Title" className="border-black border p-1"/>
-				<Button type="submit" variant="ghost">Submit</Button>
-			</form>
-			<div className="space-y-2">
-				{boards.map((board)=>(
-					<DeleteBoard id={board.id} title={board.title} key={board.id}/>
-				))}
+		<div className="w-full mb-20">
+			<Info/>
+			<Separator className="my-4"/>
+			<div className="px-2 md:px-4">
+				<BoardList/>
 			</div>
 		</div>
 	);
---
+++ components/form/form-submit.tsx
@@ -0,0 +1,20 @@
+"use client"
+import React from 'react';
+import {useFormStatus} from "react-dom";
+import {Button} from "@/components/ui/button";
+import {cn} from "@/lib/utils";
+
+interface FormSubmitProps {
+	children: React.ReactNode;
+	disabled?: boolean;
+	className?: string;
+	variant?: "default" | "destructive" | "outline" | "secondary" | "ghost" | "link" | "primary"
+}
+export const FormSubmit = ({children, variant, className, disabled}:FormSubmitProps) => {
+	const {pending}= useFormStatus();
+	return (
+		<Button className={cn(className)} disabled={pending || disabled} type="submit" variant={variant} size="sm">
+			{children}
+		</Button>
+	);
+};
